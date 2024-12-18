2027ddcc0697d3b41c7ce66733087e73b2c49d1c
Gui Gui <gui.emanuelcesar@gmail.com>
2024-12-18 20:36:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace separação_de_par_e_impar
+{
+    class Numero()
+    {
+       private int geraNumero()
+       {
+           Random gerador = new Random();
+           
+           int Numero = gerador.Next(0,101);
+
+           return Numero;
+       }
+
+       public int numeros_gerados => geraNumero();
+
+    }
+    class Par()
+    {
+       public static void numero_par()
+        {
+            Numero n = new Numero();
+
+            for (int i = 0; i< 100; i++)
+            {
+                int numeros = n.numeros_gerados;
+                if (numeros %2 ==0)
+                {
+                    Console.WriteLine(numeros);
+                }
+            }
+
+        
+        }
+
+        public static void numero_impar()
+        {
+             Numero n = new Numero();
+            for (int i = 0; i< 100; i++)
+            {
+                int numeros = n.numeros_gerados;
+                if (numeros %2 ==1)
+                {
+                    Console.WriteLine(numeros);
+                }
+        }
+
+        }
+
+    }
+}
---
+++ numeros.cs
@@ -0,0 +1,19 @@
+using System;
+using separação_de_par_e_impar;
+
+namespace visão 
+{
+    class numeros_par: Par
+    {
+        static void Main()
+        {
+
+            Console.Write("\nPar:\n");
+            numero_par();
+
+            Console.Write("\nImpar:\n");
+            numero_impar();
+        }
+    } 
+
+}
